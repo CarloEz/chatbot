e05234f769e85a950044651fd5055f08d7c54bf5
CarloEz <marcarlos47@gmail.com>
2020-05-25 18:52:40 -0600

    formato fecha

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,14 +1,16 @@
 from django.http import JsonResponse
+from datetime import datetime
 import json
 import requests
-
 
 def CasosGuatemala(request):
     result=requests.get('https://covid-api.com/api/reports?q=Guatemala%20&iso=GTM&region_name=Guatemala')
     objetos=result.json()
+
+    dateactual=datetime.strptime(objetos["data"][0]["date"],'%Y-%m-%d')
     data={
         "set_attributes":{
-        "Fecha":objetos["data"][0]["date"],
+        "Fecha":dateactual.strftime("%d-%m-%Y"),
         "Total":objetos["data"][0]["confirmed"],
         "Fallecidos":objetos["data"][0]["deaths"],
         "Recuperados":objetos["data"][0]["recovered"],
